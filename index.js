cbbaebf9d4802820a8388f79a01c2c74d0923a46
bjegan92-saineha <bjegan92@gmail.com>
2026-09-16 22:09:43 +0530

    Fix welcome message typo in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Welcome Welcome to Github new Actionssss's"});
+  res.status(200).json({ status: "ok", message: "Welcome to Github new Actionssss's"});
 });
 
 app.get("/health", (req, res) => {
