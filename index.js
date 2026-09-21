448382d1a2aadf113c49912b276d928e0d093c15
bjegan92-saineha <bjegan92@gmail.com>
2026-09-21 19:12:51 +0530

    Update welcome message in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Welcome to Github new Actions"});
+  res.status(200).json({ status: "ok", message: "Welcome to Github new Actionssssssss"});
 });
 
 app.get("/health", (req, res) => {
